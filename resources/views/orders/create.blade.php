9d1004e7a319f7938cca5143c652e695de519dca
MaysaShad <shadurdyyewamaysa@gmail.com>
2024-04-30 11:54:17 +0500

    15.0

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -2,21 +2,24 @@
 @section('content')
 
 <div class="container-xl">
-<div class="card mt-5 shadow shadow-xl">
-  <div class="card-header h3 fw-semibold">Order's Page</div>
-  <div class="card-body">
-
-    <form action="{{ url('orders') }}" method="post">
-      {!! csrf_field() !!}
-      <label class="h5 fw-semibold">Name</label><br>
-      <input type="text" name="name" id="name" class="form-control" placeholder="Name"><br>
-      <label class="h5 fw-semibold">Address</label><br>
-      <input type="text" name="address" id="address" class="form-control" placeholder="Address"><br>
-      <label class="h5 fw-semibold">Mobile</label><br>
-      <input type="text" name="mobile" id="mobile" class="form-control" placeholder="Mobile Number"><br>
-      <input type="submit" value="Save" class="btn btn-success"><br>
-    </form>
-  </div>
-</div>
+    <div class="row justify-content-center">
+      <div class="col-10">
+          <div class="card mt-5 shadow shadow-xl">
+              <div class="card-header h3 fw-semibold">Order's Page</div>
+                <div class="card-body">
+                  <form action="{{ url('orders') }}" method="post">
+                      @csrf
+                      <label class="h5 fw-semibold">Name</label><br>
+                      <input type="text" name="name" id="name" class="form-control" placeholder="Name"><br>
+                      <label class="h5 fw-semibold">Address</label><br>
+                      <input type="text" name="address" id="address" class="form-control" placeholder="Address"><br>
+                      <label class="h5 fw-semibold">Mobile</label><br>
+                      <input type="text" name="mobile" id="mobile" class="form-control" placeholder="Mobile Number"><br>
+                      <input type="submit" value="Save" class="btn btn-success"><br>
+                    </form>
+                  </div>
+              </div>
+         </div>
+    </div>
 </div>
 @stop
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -2,16 +2,20 @@
 @section('content')
 
 <div class="container-xl">
-    <div class="card mt-5 shadow shadow-xl">
-        <div class="card-header h3 fw-semibold">
-            Order's Page
-        </div>
-        <div class="card-body">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <div class="card mt-5 shadow shadow-xl">
+                <div class="card-header h3 fw-semibold">
+                    Order's Page
+                </div>
+                <div class="card-body">
 
-            <div class="card-body">
-                <h5 class="card-title">Name : {{ $orders->name }}</h5>
-                <p class="card-text">Address : {{ $orders->address }}</p>  
-                <p class="card-text">Mobile : {{ $orders->mobile}}</p>
+                    <div class="card-body">
+                        <h5 class="card-title">Name : {{ $orders->name }}</h5>
+                        <p class="card-text">Address : {{ $orders->address }}</p>  
+                        <p class="card-text">Mobile : {{ $orders->mobile}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
